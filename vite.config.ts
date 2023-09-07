1dd710a9bd48bd4fdbbb2f2543eff39b89bf70f2
Adeyemi-theRustyDev <abiadeabdulazeez@gmail.com>
2023-09-07 11:43:52 +0100

    Added github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/Adeyemi-theRustyDev/fem_news_page.git",
   plugins: [react()],
 })
